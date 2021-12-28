39766ad053e5056da72064c361096c829f9ae3cb
Sangwoo <tkddn2075@naver.com>
2021-12-29 08:35:35 +0900

    1229코테

---
+++ 숫자문자열과영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    number = ["zero", "one", "two", "three", "four",
+              "five", "six", "seven", "eight", "nine"]
+    answer = 0
+    for i in number:
+        s = s.replace(i, )
+    return answer
